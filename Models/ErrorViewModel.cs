35bf5e589af367832840a9d0702633e0308ae30c
Alexsandro Tirloni <tirloni.rs@gmail.com>
2022-11-26 20:40:14 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SB_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
